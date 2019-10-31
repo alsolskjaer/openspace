b0336e8d6b448bfa3f17e3eb7423e942b9aa0922
al solskjaer <alsolkajer@gmail.com>
2019-10-31 23:43:05 +0900

    commit 2

--- todolistAPI/settings.py
+++ todolistAPI/settings.py
@@ -79,12 +79,10 @@ WSGI_APPLICATION = 'todolistAPI.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+                default='sqlite:///{0}'.format(location('db.sqlite3')),
+                conn_max_age=0)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
